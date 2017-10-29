f830d58510cd52ded4cafe16f2e7112bd4bdf0e7
valendea <valendea.amalia@yahoo.com>
2017-10-29 18:53:24 +0800

    Fix Facebook login

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,8 +1,12 @@
 # In config/initializers/omniauth.rb
 Rails.application.config.middleware.use OmniAuth::Builder do
  	provider :facebook, ENV['FACEBOOK_APP_KEY'], ENV['FACEBOOK_APP_SECRET'], info_fields: "first_name, last_name, email"
+ 	# provider :google_oauth2, ENV['GOOGLE_CLIENT_ID'], ENV['GOOGLE_CLIENT_SECRET_ID'], info_fields: "first_name, last_name, email"
+end
+
+Rails.application.config.middleware.use OmniAuth::Builder do
+ 	# provider :facebook, ENV['FACEBOOK_APP_KEY'], ENV['FACEBOOK_APP_SECRET'], info_fields: "first_name, last_name, email"
  	provider :google_oauth2, ENV['GOOGLE_CLIENT_ID'], ENV['GOOGLE_CLIENT_SECRET_ID'], info_fields: "first_name, last_name, email"
- 	# info_fields: "auth.info.first_name, auth.info.last_name, auth.info.email"
 end
 
 # OmniAuth.config.logger = Rails.logger
@@ -10,3 +14,5 @@ end
 # Rails.application.config.middleware.use OmniAuth::Builder do
 #   provider :google_oauth2, ENV['GOOGLE_CLIENT_ID'], ENV['GOOGLE_CLIENT_SECRET_ID'], {client_options: {ssl: {ca_file: Rails.root.join("cacert.pem").to_s}}}
 # end
+
+# info_fields: "auth.info.first_name, auth.info.last_name, auth.info.email"
