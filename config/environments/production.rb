54ee28f4df6fd3e7cd1becd0919f974416ce1fde
valendea <valendea.amalia@yahoo.com>
2017-11-03 17:46:51 +0800

    Fix production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,24 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
+  #mailer setting starts
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address:                'smtp.gmail.com',
+    port:                   25,
+    user_name:              ENV["GMAIL_USERNAME"],
+    password:               ENV["GMAIL_PASSWORD"],
+    authentication:         'plain',
+    enable_starttls_auto:   true
+
+  }
+
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.default_options = {from: 'valendeatest@gmail.com'}
+  config.action_mailer.default_url_options = { :host => "https://my-airbnb.herokuapp.com/" }
+  #mailer setting ends
+
   # Code is not reloaded between requests.
   config.cache_classes = true
 
@@ -88,4 +106,6 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+
 end
